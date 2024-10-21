a78837b0f1775bf75bd087a20047633f36d0f4bf
ikkerdrgz <iker.rodriguez.perez.2005@gmail.com>
2024-10-21 14:15:16 +0200

    otra actualizacion

--- Acceso de datos T2/src/biblioteca/insercionLibro.java
+++ Acceso de datos T2/src/biblioteca/insercionLibro.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package biblioteca;
+
 import java.sql.*;
 import java.util.Scanner;
 
@@ -11,11 +12,11 @@ import java.util.Scanner;
  * @author FP
  */
 public class insercionLibro {
+
     public static final String cadenaConexion = "jdbc:mysql://localhost:3306/biblioteca";
+
     public static void visualizarLibros() {
-        try (Connection con = DriverManager.getConnection(cadenaConexion,"root", "fp.2023");
-                Statement sentencia = con.createStatement();
-                ) {
+        try (Connection con = DriverManager.getConnection(cadenaConexion, "root", "fp.2023"); Statement sentencia = con.createStatement();) {
             String selectLibro = "SELECT * FROM Libro NATURAL JOIN (Autor, Editorial, Tema)";
             ResultSet sentenciaSelect = sentencia.executeQuery(selectLibro);
             while (sentenciaSelect.next()) {
@@ -34,34 +35,66 @@ public class insercionLibro {
             System.out.println(e.getMessage());
         }
     }
-    
-    
+
     public static void main(String[] args) throws SQLException {
         String cadenaConexion = "jdbc:mysql://localhost:3306/biblioteca";
-        try (Connection con = DriverManager.getConnection(cadenaConexion,"root", "fp.2023");
-                Statement sentencia = con.createStatement();
-                ) {
-            System.out.println("Conexcion correcta");
-            
+        try (Connection con = DriverManager.getConnection(cadenaConexion, "root", "fp.2023"); Statement sentencia = con.createStatement();) {
             String isbn = "123";
             String titulo = "";
             int numeroEjemplares = 0;
             String nombreAutor = "";
             String nombreEditorial = "";
             String nombreTema = "";
-            
+
             String selectLibro = "SELECT * FROM Libro WHERE isbn = '" + isbn + "';";
             ResultSet sentenciaSelect = sentencia.executeQuery(selectLibro);
-            if(sentenciaSelect.next()) {
+            if (sentenciaSelect.next()) {
                 System.out.println("El libro ya existe");
                 visualizarLibros();
             } else {
+                if (!existeAutor(nombreAutor, con)) {
+                    altaAutor(nombreAutor, con);
+                }
+                int idAutor = buscarAutor(nombreAutor, con);
+
+                if (!existeTema(nombreTema)) {
+                    altaTema(nombreTema);
+                }
+                int idTema = buscarTema(nombreTema);
+
+                if (!existeEditorial(nombreEditorial)) {
+                    altaEditorial(nombreEditorial);
+                }
+                int idEditorial = buscarEditorial(nombreEditorial);
+
+                altaLibro(isbn, titulo, numeroEjemplares, idAutor, idEditorial, idTema);
                 visualizarLibros();
             }
-            
+
         } catch (SQLException e) {
             System.out.println("Conexcion incorrecta");
             System.out.println(e.getMessage());
         }
+    }
+
+    private static boolean existeAutor(String nombreAutor, Connection con) throws SQLException {
+        String sentenciaAutor = "SELECT * FROM autor WHERE nombreAutor = '" + nombreAutor + "'";
+        ResultSet sentenciaSelect = con.createStatement().executeQuery(sentenciaAutor);
+        if (!sentenciaSelect.next()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    private static void altaAutor(String nombreAutor, Connection con) throws SQLException {
+        String sentenciaInsert = "INSERT INTO autor (nombreAutor) values (" + nombreAutor + ")";
+        con.createStatement().executeUpdate(sentenciaInsert);
+    }
+
+    private static int buscarAutor(String nombreAutor, Connection con) throws SQLException {
+        String sentenciaAutor = "SELECT * FROM autor WHERE nombreAutor = '" + nombreAutor + "'";
+        ResultSet sentenciaSelect = con.createStatement().executeQuery(sentenciaAutor);
+        return sentenciaSelect.getInt("idAutor");
     }
 }
--- Iker_parejas/src/cartasEmparejadas/Ventana.java
+++ Iker_parejas/src/cartasEmparejadas/Ventana.java
@@ -6,12 +6,15 @@ package cartasEmparejadas;
 
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 
 /**
  *
@@ -21,14 +24,12 @@ public class Ventana {
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
     
-    JTextArea area = new JTextArea();
-    
     ArrayList<Cartas> baraja = new ArrayList<>();
     
     public Ventana() {
         frame.setVisible(true);
-        frame.setTitle("Test Hollow Knight");
-        frame.setBounds(500, 200, 900, 700);
+        frame.setTitle("");
+        frame.setBounds(700, 200, 515, 500);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
@@ -37,7 +38,7 @@ public class Ventana {
         panel.setLayout(null);
         frame.add(panel);
         
-        FondoPantalla();
+        CrearBaraja();
         panel.updateUI();
     }
     
@@ -54,23 +55,90 @@ public class Ventana {
     public void CrearBaraja() {
         for (int i = 0; i < 6; i++) {
             if (i < 2) {
-                baraja.add(new Cartas(i,"/Imagenes/reverso.jpg", "/Imagenes/bart.png"));
+                baraja.add(new Cartas(i,"imagenes/reverso.jpg", "imagenes/scraggy.jpg"));
             }
             if (i > 1 && i < 4) {
-                baraja.add(new Cartas(i,"/Imagenes/reverso.jpg", "/Imagenes/homer.png"));
+                baraja.add(new Cartas(i,"imagenes/reverso.jpg", "imagenes/raikou.png"));
             }
             if (i > 3) {
-                baraja.add(new Cartas(i,"/Imagenes/reverso.jpg", "/Imagenes/marge.png"));
+                baraja.add(new Cartas(i,"imagenes/reverso.jpg", "imagenes/sylveon.jpg"));
             }
         }
         
         JLabel carta1 = new JLabel();
+        JLabel carta2 = new JLabel();
+        JLabel carta3 = new JLabel();
+        JLabel carta4 = new JLabel();
+        JLabel carta5 = new JLabel();
+        JLabel carta6 = new JLabel();
         
-        ImageIcon imagen = new ImageIcon(baraja.get(0).cara);
+        ImageIcon imagen = new ImageIcon(baraja.get(2).reverso);
         
         carta1.setBounds(50, 50, 100, 150);
         carta1.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(carta1.getWidth(), carta1.getHeight(), Image.SCALE_DEFAULT)));
         carta1.setOpaque(true);
+        
+        carta2.setBounds(200, 50, 100, 150);
+        carta2.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(carta2.getWidth(), carta2.getHeight(), Image.SCALE_DEFAULT)));
+        carta2.setOpaque(true);
+        
+        carta3.setBounds(350, 50, 100, 150);
+        carta3.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(carta3.getWidth(), carta3.getHeight(), Image.SCALE_DEFAULT)));
+        carta3.setOpaque(true);
+        
+        carta4.setBounds(50, 250, 100, 150);
+        carta4.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(carta4.getWidth(), carta4.getHeight(), Image.SCALE_DEFAULT)));
+        carta4.setOpaque(true);
+        
+        carta5.setBounds(200, 250, 100, 150);
+        carta5.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(carta5.getWidth(), carta5.getHeight(), Image.SCALE_DEFAULT)));
+        carta5.setOpaque(true);
+        
+        carta6.setBounds(350, 250, 100, 150);
+        carta6.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(carta6.getWidth(), carta6.getHeight(), Image.SCALE_DEFAULT)));
+        carta6.setOpaque(true);
+        
+        MouseListener girar = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                                
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                imagen.setImage(new ImageIcon(baraja.get(2).cara).getImage());
+                carta1.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(carta1.getWidth(), carta1.getHeight(), Image.SCALE_DEFAULT)));
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                try {
+                    Thread.sleep(1000);
+                    imagen.setImage(new ImageIcon(baraja.get(2).reverso).getImage());
+                    carta1.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(carta1.getWidth(), carta1.getHeight(), Image.SCALE_DEFAULT)));
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                
+            }
+        };
+        carta1.addMouseListener(girar);
+        
+        
         panel.add(carta1);
+        panel.add(carta2);
+        panel.add(carta3);
+        panel.add(carta4);
+        panel.add(carta5);
+        panel.add(carta6);
     }
 }
